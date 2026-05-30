2a9fa254f23f41f6bda62b04263a344205e1ced1
yerield1o <yerield1o@gmail.com>
2026-05-30 18:11:19 +0700

    Added Brands

---
+++ src/model/Brand.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Brand {
+    private int id;
+    private String name;
+
+    public Brand(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
